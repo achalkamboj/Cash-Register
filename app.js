faf20f914608996f73a6d7337bd8a42d267359e6
Achal-Kamboj <ak6861@srmist.edu.in>
2021-08-18 17:08:55 +0530

    updated html , css and javascript

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error-message");
 const noOfNotes = document.querySelector(".no-of-notes");
 
-const availableNotes = [200,500,100,20,10,5,1];
+const availableNotes = [2000,500,100,20,10,5,1];
 
 
-checkButton.addEventListener("check", function validateBillAndCashAmount() {
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
 hideMessage();
-if(billAmount.value >= 0){
+if(billAmount.value > 0){
     if(cashGiven.value >= billAmount.value){
         const amountToBeReturned = cashGiven.value - billAmount.value;
         calculateChange(amountToBeReturned);
@@ -23,6 +23,14 @@ else{
 }
 });
 
+function calculateChange(amountToBeReturned){
+    for(var i=0; i<availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned = amountToBeReturned%availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
 function hideMessage() {
     message.style.display = "none";
 }
@@ -32,10 +40,3 @@ function showMessage(msg){
     message.innerText = msg;
 }
 
-function calculateChange(amountToBeReturned){
-    for( i=0; i<availableNotes.length; i++){
-        const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
-        amountToBeReturned = amountToBeReturned%availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
-    }
-}
