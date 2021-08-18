9077fd5e50866bd4a0a34d9e2f86339946f88180
Achal-Kamboj <ak6861@srmist.edu.in>
2021-08-18 09:19:13 +0530

    Initial commit:- Html and javascript

---
+++ app.js
@@ -0,0 +1,41 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelector(".no-of-notes");
+
+const availableNotes = [200,500,100,20,10,5,1];
+
+
+checkButton.addEventListener("check", function validateBillAndCashAmount() {
+hideMessage();
+if(billAmount.value >= 0){
+    if(cashGiven.value >= billAmount.value){
+        const amountToBeReturned = cashGiven.value - billAmount.value;
+        calculateChange(amountToBeReturned);
+    }
+    else{
+        showMessage("Do You Wanna Wash Plates?");
+    }
+}
+else{
+    showMessage("Invalid Bill Amount");
+}
+});
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg;
+}
+
+function calculateChange(amountToBeReturned){
+    for( i=0; i<availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned = amountToBeReturned%availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
